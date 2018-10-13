f0b22ab9205b8bf9abdfdafdfa8132ad0f76f487
Santiago Soler <santiago.r.soler@gmail.com>
2018-10-13 20:19:41 -0300

    Added ll alias for bash

--- dotfiles/.bash/aliases.sh
+++ dotfiles/.bash/aliases.sh
@@ -1,4 +1,5 @@
 # Define aliases
 
 alias cp="cp -i"                          # confirm before overwriting something
+alias ll="ls -lh"                        # ls in list and human readable
 alias open='xdg-open'
