e2dd7b0487bada7e77ffa9eef4048908fbc74b00
Santiago Soler <santiago.r.soler@gmail.com>
2018-10-13 20:15:50 -0300

    Improved copy.sh

--- copy.sh
+++ copy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Copy dotfiles to ~
+# Regular cp options can be passed when running the script.
+# For example:
+#   $ ./copy.sh -f     # force the copy
+#   $ ./copy.sh -b     # backup files
+
 cp ~/.bashrc ~/.bashrc_dotfiles_backup
-cp -rf dotfiles/. -t ~
+cp -r $@ dotfiles/. -t ~
