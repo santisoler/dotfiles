bee7122707fa3e0e6f73fab7fce67329ec31a5ef
Santiago Soler <santiago.r.soler@gmail.com>
2018-10-15 15:12:00 -0300

    Added alert bash alias

--- dotfiles/.bash/aliases.sh
+++ dotfiles/.bash/aliases.sh
@@ -5,3 +5,8 @@ alias ll="ls -lh"                        # ls in list and human readable
 alias open='xdg-open'
 alias xc='xclip -selection clipboard'    # copy to clipboard using xclip
 alias diceware-es='diceware -d " " --no-caps $DICEWARE_ES'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
