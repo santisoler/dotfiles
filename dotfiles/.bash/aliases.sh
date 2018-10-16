a5f05d5f7c1b92da59f3958730b02bad1223528b
Santiago Soler <santiago.r.soler@gmail.com>
2018-10-16 11:48:26 -0300

    Added git misspelling aliases to .bash/aliases.sh

--- dotfiles/.bash/aliases.sh
+++ dotfiles/.bash/aliases.sh
@@ -6,6 +6,10 @@ alias open='xdg-open'
 alias xc='xclip -selection clipboard'    # copy to clipboard using xclip
 alias diceware-es='diceware -d " " --no-caps $DICEWARE_ES'
 
+# git aliases for misspelling
+alias gti="git"
+alias gi="git"
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
