514d917ff4a033939153d35e69bc41a620b48509
Santiago Soler <santiago.r.soler@gmail.com>
2021-09-10 14:08:22 -0300

    Clean bin files (#42)
    * Remove unused bin files
    
    * Change permissions of teclado
    
    * Add grabar script

--- dotfiles/bin/ssh-add-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# In order to runt this script at login, add the following to autostart:
-# xfce4-terminal -e /path/to/ssh-add-script.sh
-
-echo "Enter SSH password for ssh-agent"
-echo "--------------------------------"
-ssh-add
