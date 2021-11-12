6c1adfa4ab9413a3030d6482b25ecb4315f76653
Santiago Soler <santiago.r.soler@gmail.com>
2021-11-12 15:47:16 -0300

    Remove old dotfiles

--- dotfiles/templates/python/mypackage/__init__.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=missing-docstring
-# Import functions/classes to make the public API
-from ._version import get_versions
-
-# Get the version number through versioneer
-__version__ = get_versions()["version"]
-del get_versions
