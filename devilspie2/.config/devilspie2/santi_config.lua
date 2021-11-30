cd5e15a4f1c0276afa5a298c654cd8bcc4e81090
Santiago Soler <santiago.r.soler@gmail.com>
2021-11-30 17:03:36 -0300

    Change to workspace of the app when launched

--- devilspie2/.config/devilspie2/santi_config.lua
+++ devilspie2/.config/devilspie2/santi_config.lua
@@ -1,25 +1,33 @@
 -- Assign Terminator and Firefox to workspace 1
 if (get_application_name() == "terminator") then
   set_window_workspace(1);
+  change_workspace(1);
 end
+
+-- Assign Firefox to workspace 2
 if (get_application_name() == "Firefox") then
-  set_window_workspace(1);
-end
-
--- Assign Thunderbird to workspace 2
-if (get_application_name() == "Thunderbird") then
   set_window_workspace(2);
+  change_workspace(2);
 end
 
--- Assign Telegram and Slack to workspace 3
+-- Assign Thunderbird to workspace 3
+if (get_application_name() == "Thunderbird") then
+  set_window_workspace(3);
+  change_workspace(3);
+end
+
+-- Assign Telegram and Slack to workspace 4
 if (get_application_name() == "TelegramDesktop") then
-  set_window_workspace(3);
+  set_window_workspace(4);
+  change_workspace(4);
 end
-if (get_application_name() == "Slack") then
-  set_window_workspace(3);
+if string.match(get_application_name(), "Slack") then
+  set_window_workspace(4);
+  change_workspace(4);
 end
 
--- Assign Lollypop and Deluge to workspace 3
+-- Assign Lollypop to workspace 5
 if (get_application_name() == "Lollypop") then
-  set_window_workspace(4);
+  set_window_workspace(5);
+  change_workspace(5);
 end
