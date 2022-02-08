ea36e347dd8fea9c5dae10835fee1c9c1802a642
Santiago Soler <santiago.r.soler@gmail.com>
2022-02-08 08:00:53 -0300

    Start moving Neovim from CoC to LSP

---
+++ neovim/.config/nvim/lua/lsp.lua
@@ -0,0 +1,72 @@
+-- Configure LSP and autocompletion
+-- Required packages:
+--     'neovim/nvim-lspconfig'       " configurations for built-in LSP client
+--     'hrsh7th/nvim-cmp'            " autocompletion plugin
+--     'hrsh7th/cmp-nvim-lsp'        " LSP source for nvim-cmp
+--     'saadparwaiz1/cmp_luasnip'    " Snippets source for nvim-cmp
+--     'L3MON4D3/LuaSnip'            " Snippets plugin
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- Enable some language servers with the additional completion capabilities
+-- offered by nvim-cmp
+local servers = { 'pyright', 'bashls'}
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
+end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
