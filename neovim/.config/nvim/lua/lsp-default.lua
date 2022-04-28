53d66b5473faa49cd4c040d89f0b3e64a42f528b
Santiago Soler <santiago.r.soler@gmail.com>
2022-04-28 11:02:44 -0300

    Add more language servers

--- neovim/.config/nvim/lua/lsp-default.lua
+++ neovim/.config/nvim/lua/lsp-default.lua
@@ -28,13 +28,13 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pylsp', 'bashls' }
+local servers = { 'bashls', 'pylsp', 'texlab' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    }
+    -- flags = {
+    --   -- This will be the default in neovim 0.7+
+    --   debounce_text_changes = 150,
+    -- }
   }
 end
