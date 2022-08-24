b3b683bb1a26ec259969c59530fea1585699d5b6
Santiago Soler <santiago.r.soler@gmail.com>
2022-08-23 22:13:36 -0700

    Fix rename-home-folders.sh
    Give execution permissions and add full path to home folders.

--- rename-home-folders.sh
+++ rename-home-folders.sh
@@ -7,7 +7,7 @@ folders="Documents Downloads Music Pictures Public Templates Videos"
 for folder in $folders; do
     lowercase_name=$(echo "$folder" | tr '[:upper:]' '[:lower:]')
     # Rename each folder
-    mv "$folder" "$lowercase_name"
+    mv "$HOME/$folder" "$HOME/$lowercase_name"
     # Change folder name in ~/.config/users-dirs.dirs
     sed -i 's/'${folder}'/'${lowercase_name}'/' "$HOME/.config/user-dirs.dirs"
 done
