f5293799c016f3dff4dd0ca1bf905bafc04ae5e9
Santiago Soler <santiago.r.soler@gmail.com>
2022-09-21 16:50:01 -0700

    Set max-line-length to 88 in nvim-lint for flake8

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -1,7 +1,22 @@
 -- Configure nvim-lint
+-- -------------------
+
+-- Configure nvim-lint
 require('lint').linters_by_ft = {
   c= {'cppcheck'},
-  python = {'flake8',},
+  python = {'flake8'},
   tex = {'chktex', 'lacheck'},
   sh = {'shellcheck'},
 }
+
+-- Modify flake8 options
+local flake8 = require('lint.linters.flake8')
+flake8.args = {
+  -- <- Add a new parameter here
+  '--max-line-length=88',
+  -- <--
+  '--format=%(path)s:%(row)d:%(col)d:%(code)s:%(text)s',
+  '--no-show-source',
+  '-',
+}
+
