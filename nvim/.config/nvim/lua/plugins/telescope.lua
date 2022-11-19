47fec13252236eb8484b0dacb147174a1e1de3ad
Santiago Soler <santisoler@fastmail.com>
2022-11-19 10:55:47 -0800

    Improve telescope configuration
    Add keybinding to run git grep search

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,8 @@
 -- Configure telescope
 -- -------------------
-vim.keymap.set(
-  'n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>"
-)
-vim.keymap.set(
-  'n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>"
-)
-vim.keymap.set(
-  'n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>"
-)
-vim.keymap.set(
-  'n', '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>"
-)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fl', builtin.git_files, {})
