3882c5a7c1ff9fd5cbdf95951c6310ee0fb6a6a9
Santiago Soler <santisoler@fastmail.com>
2022-12-25 15:23:10 -0800

    Fix path to vimwiki directory
    Replace documents for Documents

--- nvim/.config/nvim/lua/santisoler/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/vimwiki.lua
@@ -1,12 +1,12 @@
 -- vimwiki
 -- -------
 vim.g.vimwiki_list = {{
-    path='~/documents/notes/vimwiki',
-    template_path='~/documents/notes/vimwiki/templates/',
+    path='~/Documents/notes/vimwiki',
+    template_path='~/Documents/notes/vimwiki/templates/',
     template_default='default',
     syntax='markdown',
     ext='.md',
-    path_html='~/documents/notes/vimwiki/site_html/',
+    path_html='~/Documents/notes/vimwiki/site_html/',
     custom_wiki2html='vimwiki_markdown',
     template_ext='.tpl',
 }}
