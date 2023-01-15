62ce67f082f11431ab82d6e5c5ca81cc227886f6
Santiago Soler <santisoler@fastmail.com>
2023-01-14 19:50:09 -0800

    Use relative path in lualine

--- nvim/.config/nvim/lua/santisoler/plugins/lualine.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/lualine.lua
@@ -30,6 +30,13 @@ require('lualine').setup {
   },
   sections = {
     lualine_b = {'branch', 'diff', getCondaEnv, 'diagnostics'},
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
+    },
     lualine_y = {getWords, 'progress'},
   },
 }
