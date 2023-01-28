c53432f86ecb7824c753f25e99f46ed67e9704e6
Santiago Soler <santisoler@fastmail.com>
2023-01-28 10:10:54 -0800

    Use same leader as the global one for vimtex

--- nvim/.config/nvim/lua/santisoler/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/vimtex.lua
@@ -3,7 +3,7 @@
 -- ------
 vim.cmd [[
   " filetype plugin indent on
-  let maplocalleader = ","
+  " let maplocalleader = ","
   let g:vimtex_view_method = 'zathura'
   let g:tex_flavor = 'latex'
   let g:vimtex_compiler_enabled=1
