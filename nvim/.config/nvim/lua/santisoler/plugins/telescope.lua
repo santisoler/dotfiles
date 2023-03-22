b77640a2bf43eb86d9a3d39954c0bbb84ecbc787
Santiago Soler <santisoler@fastmail.com>
2023-03-22 16:08:37 -0700

    Run specific telescope actions when in a git repo
    When we are working in a git repo, the find_files will be replaced by
    git_files, and live_grep will use git grep instead.
    The check for git repo is done at runtime.

--- nvim/.config/nvim/lua/santisoler/plugins/telescope.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/telescope.lua
@@ -1,33 +1,55 @@
 -- Configure telescope
 -- -------------------
-function live_git_grep()
-  -- Run live_grep with git grep
-  local prompt_title = "Git Grep"
-  local vimgrep_arguments = {
-    "git",
-     "grep",
-     "--full-name",
-     "--line-number",
-     "--column",
-     "--extended-regexp",
-     "--ignore-case",
-     "--no-color",
-     "--recursive",
-     "--recurse-submodules",
-     "-I"
-  }
-  local opts = {
-    prompt_title=prompt_title,
-    vimgrep_arguments=vimgrep_arguments,
-  }
-  require('telescope.builtin').live_grep(opts)
+function is_git_repo()
+  -- Check if we are inside a git repo
+  local status = os.execute("git rev-parse --show-toplevel &> /dev/null")
+  if status == 0 then
+    return true
+  end
+  return false
 end
 
+
+function live_git_grep()
+  -- Run live_grep with defaults or with git grep
+  if is_git_repo() then
+    local vimgrep_arguments = {
+      "git",
+       "grep",
+       "--full-name",
+       "--line-number",
+       "--column",
+       "--extended-regexp",
+       "--ignore-case",
+       "--no-color",
+       "--recursive",
+       "--recurse-submodules",
+       "-I"
+    }
+    local opts = {
+      prompt_title="Git Grep",
+      vimgrep_arguments=vimgrep_arguments,
+    }
+    require('telescope.builtin').live_grep(opts)
+  else
+    require('telescope.builtin').live_grep()
+  end
+end
+
+
+function find_files()
+  -- Find files with find_files or git_files
+  if is_git_repo() then
+    require('telescope.builtin').git_files()
+  else
+    require('telescope.builtin').find_files()
+  end
+end
+
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fq', live_git_grep, {})
-vim.keymap.set('n', '<leader>fl', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', find_files, {})
+vim.keymap.set('n', '<leader>fg', live_git_grep, {})
 vim.keymap.set('n', '<leader>fs', builtin.git_status, {})
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
@@ -43,6 +65,3 @@ require('telescope').setup({
     layout_config = { height = 0.95 },
   },
 })
-
-
-
