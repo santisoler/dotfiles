6b1c3e3ad6d7c9ebb5c2bf4b3c0f05d06d73b7d0
Santiago Soler <santisoler@fastmail.com>
2023-03-22 14:49:24 -0700

    Add custom function to run git grep in telescope

--- nvim/.config/nvim/lua/santisoler/plugins/telescope.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/telescope.lua
@@ -1,26 +1,48 @@
 -- Configure telescope
 -- -------------------
+function live_git_grep()
+  -- Run live_grep with git grep
+  local prompt_title = "Git Grep"
+  local vimgrep_arguments = {
+    "git",
+     "grep",
+     "--full-name",
+     "--line-number",
+     "--column",
+     "--extended-regexp",
+     "--ignore-case",
+     "--no-color",
+     "--recursive",
+     "--recurse-submodules",
+     "-I"
+  }
+  local opts = {
+    prompt_title=prompt_title,
+    vimgrep_arguments=vimgrep_arguments,
+  }
+  require('telescope.builtin').live_grep(opts)
+end
+
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fq', live_git_grep, {})
+vim.keymap.set('n', '<leader>fl', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', builtin.git_status, {})
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fe', builtin.resume, {})
-vim.keymap.set('n', '<leader>fl', builtin.find_files, {})
 
--- Configure layout of telescope
+
+-- Configure telescope
 require('telescope').setup({
   defaults = {
     layout_strategy = "vertical",
     layout_config = { height = 0.95 },
-    -- configure live_grep to run git grep
-    vimgrep_arguments = {
-      "git", "grep", "--full-name", "--line-number", "--column", "--extended-regexp", "--ignore-case",
-      "--no-color", "--recursive", "--recurse-submodules", "-I"
-    },
-    --
   },
 })
+
+
+
