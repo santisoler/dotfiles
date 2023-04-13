4371f934d645e928a022583c641bedf6c7959861
Santiago Soler <santisoler@fastmail.com>
2023-04-12 17:47:41 -0700

    Simplify keybinding for toggling diagnostics

--- nvim/.config/nvim/lua/santisoler/plugins/toggle-lsp-diagnostics.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/toggle-lsp-diagnostics.lua
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<leader>tls", "<Plug>(toggle-lsp-diag-signs)")
 vim.keymap.set("n", "<leader>tlv", "<Plug>(toggle-lsp-diag-vtext)")
 vim.keymap.set("n", "<leader>tlp", "<Plug>(toggle-lsp-diag-update_in_insert)")
 
-vim.keymap.set("n", "<leader>tld",  "<Plug>(toggle-lsp-diag)")
+vim.keymap.set("n", "<leader>td",  "<Plug>(toggle-lsp-diag)")
 vim.keymap.set("n", "<leader>tldd", "<Plug>(toggle-lsp-diag-default)")
 vim.keymap.set("n", "<leader>tldo", "<Plug>(toggle-lsp-diag-off)")
 vim.keymap.set("n", "<leader>tldf", "<Plug>(toggle-lsp-diag-on)")
