9a6e4d39bc611b1a4cb8fd74a7b037302d52dd9b
Santiago Soler <santisoler@fastmail.com>
2023-06-05 10:33:45 -0700

    Don't use on_attach for ruff_lsp
    Ruff language server doesn't need keybindings.

--- nvim/.config/nvim/lua/santisoler/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/lspconfig.lua
@@ -113,7 +113,6 @@ end
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
 -- For the default config, along with instructions on how to customize the settings
 require('lspconfig').ruff_lsp.setup {
-  on_attach = on_attach,
   init_options = {
     settings = {
       -- Any extra CLI arguments for `ruff` go here.
