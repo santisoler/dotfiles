c55426a8567d54861284c430874039bc1ddca603
Santiago Soler <santisoler@fastmail.com>
2024-05-15 12:22:17 -0700

    Add vimdoc to Tresitter packages

--- nvim/.config/nvim/lua/santisoler/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "python", "lua", "latex", "c", "rust" },
+  ensure_installed = { "python", "lua", "latex", "c", "rust", "vimdoc" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
