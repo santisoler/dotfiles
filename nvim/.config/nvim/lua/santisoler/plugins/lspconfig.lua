48fdf4c48ab92fb93a9627d38445d64159e4aea7
Santiago Soler <santisoler@fastmail.com>
2024-05-29 15:10:49 -0700

    Disable ruff lsp
    Disable after noticing that it makes `gq` not working in docstrings.

--- nvim/.config/nvim/lua/santisoler/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/santisoler/plugins/lspconfig.lua
@@ -78,14 +78,14 @@ end
 -- Configure `ruff-lsp`.
 -- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
 -- For the default config, along with instructions on how to customize the settings
-lspconfig.ruff_lsp.setup {
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {},
-    }
-  }
-}
+-- lspconfig.ruff_lsp.setup {
+--   init_options = {
+--     settings = {
+--       -- Any extra CLI arguments for `ruff` go here.
+--       args = {},
+--     }
+--   }
+-- }
 
 -- Texlab
 lspconfig.texlab.setup {
