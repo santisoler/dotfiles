1784117fccd04ff795880ef93982ffa8197e996e
Santiago Soler <santisoler@fastmail.com>
2025-04-06 19:16:52 -0700

    Move lazy to the bottom of init.lua
    This avoids some overrides that lazy was doing to some general
    configuration on first run.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
-require("config.lazy")
 require("config.base")
 require("config.keymappings")
 require("config.functions")
 require("config.autocmd")
 require("config.lspattach")
 require("config.diagnostics")
+require("config.lazy")
