8b7ca977214421980cb3c7f7df74b663070233a6
Santiago Soler <santisoler@fastmail.com>
2025-04-06 18:25:31 -0700

    Configure bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,14 @@
 local function config()
 	vim.opt.termguicolors = true
-	require("bufferline").setup({})
+	require("bufferline").setup({
+		highlights = require("catppuccin.groups.integrations.bufferline").get(),
+		options = {
+			offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "center" } },
+			separator_style = "thin",
+			always_show_bufferline = false,
+			show_close_icon = false,
+		},
+	})
 end
 
 return {
