b3f16636ecdaf02f023b3308ace211c9d03d00c6
Santiago Soler <santisoler@fastmail.com>
2025-04-07 14:10:48 -0700

    Add treesitter-context

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,48 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "williamboman/mason.nvim", --depend on mason to install tree-sitter first
-    },
-    build = {":MasonInstall tree-sitter-cli", ":TSUpdate"},
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+local function config_treesitter()
+	local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = {
-            "python",
-            "lua",
-            "c",
-            "rust",
-            "latex",
-            "vimdoc",
-            "html",
-          },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
-   }
- }
+	configs.setup({
+		ensure_installed = {
+			"python",
+			"lua",
+			"c",
+			"rust",
+			"latex",
+			"vimdoc",
+			"html",
+		},
+		sync_install = false,
+		highlight = { enable = true },
+		indent = { enable = true },
+	})
+end
+
+local function config_treesitter_context()
+	-- Configure treesitter-context
+	require("treesitter-context").setup({
+		multiline_threshold = 1, -- Maximum number of lines to show for a single context
+	})
+
+	-- Set keybinding to go to last context
+	vim.keymap.set("n", "[n", function()
+		require("treesitter-context").go_to_context()
+	end, { silent = true })
+end
+
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"williamboman/mason.nvim", --depend on mason to install tree-sitter first
+		},
+		build = { ":MasonInstall tree-sitter-cli", ":TSUpdate" },
+		config = config_treesitter,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = config_treesitter_context,
+	},
+}
